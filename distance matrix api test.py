f1c2aa8233c6701aeaf7bf935195c82ff8c29761
Interspectral6 <tianle06@163.com>
2024-09-24 00:53:46 -0500

    Create distance matrix api test.py

---
+++ distance matrix api test.py
@@ -0,0 +1,15 @@
+import requests
+
+def get_walk_time(origin, destination, api_key):
+    url = f"https://maps.googleapis.com/maps/api/distancematrix/json?origins={origin}&destinations={destination}&mode=walking&key={api_key}"
+    response = requests.get(url).json()
+    if response["status"] == "OK":
+        #print(response)
+        return response["rows"][0]["elements"][0]["duration"]["text"]
+    else:
+        return "Error"
+
+if __name__ == "__main__":
+    print(get_walk_time("Illini Union", "Grainger Library", "AIzaSyDdPQv02MWF7RuEzp-E-n7nHF_YBhSemlc"))
+
+#Should work now
